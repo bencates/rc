740ea29db604c9d6d0fa0ac5f17eb3fd2abef7cd
Ben Cates <ben@mojotech.com>
2019-07-04 15:50:06 -0600

    Create react app

--- examples/chat/config/dev.exs
+++ examples/chat/config/dev.exs
@@ -13,10 +13,8 @@ config :chat, ChatWeb.Endpoint,
   check_origin: false,
   watchers: [
     node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
+      "node_modules/react-scripts/bin/react-scripts.js",
+      "start",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
