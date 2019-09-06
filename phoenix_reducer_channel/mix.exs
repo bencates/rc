f4b19e4883b558ed7128040b464e157f8c1c5d7a
Ben Cates <ben@mojotech.com>
2019-09-06 11:03:07 -0600

    Publish

--- phoenix_reducer_channel/mix.exs
+++ phoenix_reducer_channel/mix.exs
@@ -5,9 +5,12 @@ defmodule RC.MixProject do
     [
       app: :phoenix_reducer_channel,
       version: "0.1.0",
+      description: "Server-side Redux reducers over Phoenix channels",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/bencates/rc/tree/master/phoenix_reducer_channel"
     ]
   end
 
@@ -24,6 +27,13 @@ defmodule RC.MixProject do
       {:phoenix, "~> 1.4.7", optional: true},
       {:jason, "~> 1.1.2", optional: true},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bencates/rc"}
     ]
   end
 end
